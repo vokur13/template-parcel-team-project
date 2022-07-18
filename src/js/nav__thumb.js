4646df86c84dd2380e911d6c19da1f9fba4366e9
vokur13 <vokur13@gmail.com>
2022-07-18 19:57:40 +0300

    general outlay according to template complete

--- src/js/nav__thumb.js
+++ src/js/nav__thumb.js
@@ -4,16 +4,22 @@ const refs = {
   openModalBtn: document.querySelector('[data-menu-open]'),
   closeModalBtn: document.querySelector('[data-menu-close]'),
   backdrop: document.querySelector('[data-nav-thumb]'),
+  navEnv: document.querySelector('[data-nav-env]'),
+  modalButton: document.querySelector('[data-mdl-btn]'),
 };
 
 refs.openModalBtn.addEventListener('click', toggleModal);
 refs.closeModalBtn.addEventListener('click', toggleModal);
 
 refs.backdrop.addEventListener('click', logBackdropClick);
+refs.navEnv.addEventListener('click', logBackdropClick);
+refs.modalButton.addEventListener('click', logBackdropClick);
 
 function toggleModal() {
   document.body.classList.toggle('menu-open');
-  refs.backdrop.classList.toggle('is-hidden');
+  refs.backdrop.classList.toggle('is-open');
+  refs.navEnv.classList.toggle('is-open');
+  refs.modalButton.classList.toggle('is-open');
 }
 
 function logBackdropClick() {
