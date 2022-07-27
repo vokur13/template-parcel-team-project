7845d0b261b04b522cf791e8ae36f5abf5ae4d36
vokur13 <103522917+vokur13@users.noreply.github.com>
2022-07-27 16:10:42 +0000

    Deploying to gh-pages from @ vokur13/template-parcel-team-project@e3b70e368c0a8a0cebf7c2d68b801df970b5c647 🚀

--- index.9dfb92d3.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openMenuBtn:document.querySelector("[data-menu-open]"),closeMenuBtn:document.querySelector("[data-menu-close]"),menuBackdrop:document.querySelector("[data-thumb]")};function n(){document.body.classList.toggle("menu-open"),e.menuBackdrop.classList.toggle("is-open")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
-//# sourceMappingURL=index.9dfb92d3.js.map
