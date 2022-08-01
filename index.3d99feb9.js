4325c83b830f627bcca6e513b8a99020a14515d7
vokur13 <103522917+vokur13@users.noreply.github.com>
2022-08-01 16:33:15 +0000

    Deploying to gh-pages from @ vokur13/template-parcel-team-project@3f8285c123a48c67c0f7b179f87d9ba720709c08 🚀

--- index.3d99feb9.js
+++
@@ -1,2 +0,0 @@
-$(document).ready((function(){$(".header__burger").click((function(e){$(".header__burger, .header__menu").toggleClass("active"),$("body").toggleClass("lock")}))}));
-//# sourceMappingURL=index.3d99feb9.js.map
--- index.84311996.js
+++
@@ -1,2 +0,0 @@
-$(document).ready((function(){$(".header__burger").click((function(e){$(".header__burger, .header__menu").toggleClass("active"),$("body").toggleClass("lock")}))}));
-//# sourceMappingURL=index.84311996.js.map
